d51a07469365c1c2ec86d2f23f60c44c02c13b6f
EGOIST <0x142857@gmail.com>
2024-10-30 14:57:48 +0800

    more openai and claude models

--- src/renderer/src/lib/openai.ts
+++ src/renderer/src/lib/openai.ts
@@ -1,6 +1,30 @@
 import { Config, ConnectionType } from '@shared/types'
-import OpenAI from 'openai'
-import Anthropic from '@anthropic-ai/sdk'
+import { generateText } from 'ai'
+import { createAnthropic } from '@ai-sdk/anthropic'
+import { createOpenAI } from '@ai-sdk/openai'
+import { ModelId, models } from '@shared/constants'
+
+const createProvider = (config: Config, model: ModelId) => {
+  if (model.startsWith('claude-')) {
+    if (!config.anthropicApiKey) {
+      throw new Error('Missing Anthropic API Key')
+    }
+
+    return createAnthropic({
+      apiKey: config.anthropicApiKey,
+      baseURL: config.anthropicApiEndpoint
+    })
+  }
+
+  if (!config.openaiApiKey) {
+    throw new Error('Missing OpenAI API Key')
+  }
+
+  return createOpenAI({
+    baseURL: config.openaiApiEndpoint,
+    apiKey: config.openaiApiKey || ''
+  })
+}
 
 const sendMessages = async ({
   config,
@@ -8,56 +32,23 @@ const sendMessages = async ({
   maxTokens
 }: {
   config: Config
-  messages: { role: 'user' | 'assistant'; content: string }[]
+  messages: { role: 'user' | 'assistant' | 'system'; content: string }[]
   maxTokens?: number
 }) => {
-  const model = config.model || 'gpt-3.5-turbo'
+  const modelId = config.model || 'gpt-3.5-turbo'
 
-  let content = ''
+  const provider = createProvider(config, modelId)
 
-  if (model.startsWith('claude-3')) {
-    if (!config.anthropicApiKey) {
-      throw new Error('Missing Anthropic API Key')
-    }
+  const model = models.find((m) => m.value === modelId)!
 
-    const anthropic = new Anthropic({
-      apiKey: config.anthropicApiKey,
-      baseURL: config.anthropicApiEndpoint
-    })
+  const result = await generateText({
+    model: provider(model.realModelId || model.value),
+    maxTokens: maxTokens || 1024,
+    messages,
+    temperature: 0
+  })
 
-    const msg = await anthropic.messages.create({
-      model:
-        model === 'claude-3-haiku'
-          ? 'claude-3-haiku-20240307'
-          : model === 'claude-3-sonnet'
-            ? '	claude-3-sonnet-20240229'
-            : 'claude-3-opus-20240229',
-      max_tokens: maxTokens || 1024,
-      messages
-    })
-
-    content = msg.content[0].text
-  } else {
-    if (!config.openaiApiKey) {
-      throw new Error('Missing OpenAI API Key')
-    }
-
-    const openai = new OpenAI({
-      baseURL: config.openaiApiEndpoint,
-      apiKey: config.openaiApiKey || '',
-      dangerouslyAllowBrowser: true
-    })
-
-    const result = await openai.chat.completions.create({
-      model: model === 'gpt-3.5-turbo' ? 'gpt-4-turbo-preview' : model,
-      max_tokens: maxTokens,
-      messages
-    })
-
-    content = result.choices[0].message.content?.trim() || ''
-  }
-
-  return { content }
+  return { content: result.text }
 }
 
 export const generateSQL = async (options: {
@@ -69,14 +60,11 @@ export const generateSQL = async (options: {
   const content = [
     `Database type: ${options.connecttionType}\n`,
     options.schema ? `Database schema:\n${options.schema}\n\n` : '',
-    `Generate an SQL query${options.schema ? ' based on provided schema' : ''} for the question: ${options.input}\n\nReturn the SQL directly without any other text, do NOT wrap in code block, table name and column name should be escaped with quotes. If you can determine the SQL simply reply "I don't know".`
+    `Generate an SQL query${options.schema ? ' based on provided schema' : ''} for the question: ${options.input}\n\nReturn the SQL directly without any other text, do NOT wrap in code block, table name and column name should be escaped with quotes.`
   ].join('')
-
-  const maxTokens = 1024
 
   const output = await sendMessages({
     config: options.config,
-    maxTokens,
     messages: [{ role: 'user', content: content }]
   })
 
@@ -93,10 +81,8 @@ export const generateFixForError = async (options: {
     ` Generate a fix for the following error when querying a ${options.type} database:\n\n${options.error}\n\nThe query that caused the error is:\n\n${options.query}\n\nReturn in the following forma:\nCorrect query:\n\`\`\`\n<correct query>\n\`\`\`\n\n<reason>`
   ].join('')
 
-  const maxTokens = 1024
   const output = await sendMessages({
     config: options.config,
-    maxTokens,
     messages: [{ role: 'user', content: content }]
   })
 
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -5,19 +5,36 @@ export const models = [
   },
   {
     label: 'GPT-4 Turbo',
-    value: 'gpt-4-turbo' as const
+    value: 'gpt-4-turbo' as const,
+    realModelId: 'gpt-4-turbo-preview'
+  },
+  {
+    label: 'GPT-4o',
+    value: 'gpt-4o' as const
+  },
+  {
+    label: 'GPT-4o mini',
+    value: 'gpt-4o-mini' as const
   },
   {
     label: 'Claude 3 Haiku',
-    value: 'claude-3-haiku' as const
+    value: 'claude-3-haiku' as const,
+    realModelId: 'claude-3-haiku-20240307'
   },
   {
     label: 'Claude 3 Sonnet',
-    value: 'claude-3-sonnet' as const
+    value: 'claude-3-sonnet' as const,
+    realModelId: 'claude-3-sonnet-20240229'
   },
   {
     label: 'Claude 3 Opus',
-    value: 'claude-3-opus' as const
+    value: 'claude-3-opus' as const,
+    realModelId: 'claude-3-opus-latest'
+  },
+  {
+    label: 'Claude 3.5 Sonnet',
+    value: 'claude-3.5-sonnet' as const,
+    realModelId: 'claude-3-5-sonnet-latest'
   }
 ]
 
